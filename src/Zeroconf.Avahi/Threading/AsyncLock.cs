d6fac89aadd65871167ab63f6da0c8f9ec8866e7
Holger Böhnke <holger.boehnke@amarin.de>
2022-05-29 20:49:35 +0200

    Zeroconf: split the AsyncLock into a normal and debug extension #61331

--- src/Zeroconf.Avahi/Threading/AsyncLock.cs
+++ src/Zeroconf.Avahi/Threading/AsyncLock.cs
@@ -17,16 +17,38 @@ public class AsyncLock : SemaphoreSlim
 
 public static class AsyncLockExtension
 {
-    public static async Task<IDisposable> Enter(this AsyncLock semaphore, string? description = null)
+    public static async Task<IDisposable> Enter(this AsyncLock semaphore)
     {
-        var cleanedDescription = string.IsNullOrEmpty(description) ? string.Empty : " " + description;
-        semaphore.Logger?.LogTrace("enter semaphore{Description}", cleanedDescription);
-
-        var wrapper = new LockHolder(semaphore, cleanedDescription);
+        var wrapper = new LockHolder(semaphore);
         await wrapper.Semaphore.WaitAsync().ConfigureAwait(false);
+        return wrapper;
+    }
 
-        semaphore.Logger?.LogTrace("semaphore entered{Description}", cleanedDescription);
+    private sealed class LockHolder : IDisposable
+    {
+        public AsyncLock Semaphore { get; }
 
+        public LockHolder(AsyncLock semaphore)
+        {
+            this.Semaphore = semaphore;
+        }
+
+        public void Dispose()
+        {
+            this.Semaphore.Release();
+        }
+    }
+}
+
+public static class AsyncLockDebugExtension
+{
+
+public static async Task<IDisposable> Enter(this AsyncLock semaphore, string description)
+    {
+        semaphore.Logger?.LogTrace("enter semaphore {Description}", description);
+        var wrapper = new LockHolder(semaphore, description);
+        await wrapper.Semaphore.WaitAsync().ConfigureAwait(false);
+        semaphore.Logger?.LogTrace("semaphore entered {Description}", description);
         return wrapper;
     }
 
@@ -37,13 +59,13 @@ public static class AsyncLockExtension
 
         public LockHolder(AsyncLock semaphore, string description)
         {
-            this.Semaphore = semaphore;
+            this.Semaphore = semaphore; 
             this.description = description;
         }
 
         public void Dispose()
         {
-            this.Semaphore.Logger?.LogTrace("leave semaphore{Description}", this.description);
+            this.Semaphore.Logger?.LogTrace("leave semaphore {Description}", this.description);
             this.Semaphore.Release();
         }
     }
